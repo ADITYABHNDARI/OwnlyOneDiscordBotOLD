ef0b2a522fb1ae163cd4b846c049769e2872b0e0
Curios Basant <basantbrpl@gmail.com>
2020-05-05 11:35:42 +0530

    required dotenv

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-// require('dotenv').config();
+require('dotenv').config();
 const bot = require('./ownly_one.js');
-bot.login(process.env.BOT_TOKEN);
+
+console.log(process.env.BOT_TOKEN);
+const token = process.env.BOT_TOKEN || "Njk2MDYyNTM1MDA2MTU4OTI4.Xq0g0Q.jy9NymP5NgbMzo0PORWCmn4fqi4";
+bot.login(token);
